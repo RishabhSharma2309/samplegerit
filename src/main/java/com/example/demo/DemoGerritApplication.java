c4af32a20c4948673edac33caca990b74e52d1c8
Rishabh <rishabh.sharma@brightlifecare.com>
2021-04-05 14:02:03 -0400

    feat: making initial commit

---
+++ src/main/java/com/example/demo/DemoGerritApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGerritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGerritApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
