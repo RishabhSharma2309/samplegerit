1d9346c241b402dab33958a9b9e6336deb186bbb
Rishabh <rishabh.sharma@brightlifecare.com>
2021-04-08 03:33:27 -0400

    ans

--- src/main/java/com/example/demo/DemoGerritApplication.java
+++ src/main/java/com/example/demo/DemoGerritApplication.java
@@ -8,8 +8,13 @@ public class DemoGerritApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGerritApplication.class, args);
-		System.out.println("hello world");
+		System.out.println("hello worlsd");
 		System.out.println("git integrated");
+		System.out.println("gerrit integrated2");
+		System.out.println("gerrit integrated3");
+		System.out.println("gerrit integrated4");
+		System.out.println("gerrit integrated6");
+		System.out.println("gerrit integrated7");
 	}
 
 }
